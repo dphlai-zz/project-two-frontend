01134f375da157f8831dd363b1326645b6450457
Danny Lai <dannyphlai@gmail.com>
2020-08-31 12:25:55 +1000

    added sprite left, right, turn animations

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function preload ()
 );
 }
 
-let platforms; 
+let platforms;
 
 function create ()
 {
@@ -43,7 +43,33 @@ function create ()
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
 
-  console.log(platforms)
+  // console.log(platforms)
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{key: 'dude', frame: 4}],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+    frameRate: 10,
+    repeat: -1
+  });
+
 }
 
 function update ()
